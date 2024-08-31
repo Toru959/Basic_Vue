11e0946b702cbacfd8a6891ee0d5effa31cac3f1
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-08-31 12:02:39 +0800

    マルチページモード

---
+++ section06/multipage/src/index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="app">
+        Index<br>
+        <a href="index.html">index</a><br>
+        <a href="users.html">users</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IndexPage' // 変更: 複数単語にする
+}
+</script>
+
+<style>
+
+</style>

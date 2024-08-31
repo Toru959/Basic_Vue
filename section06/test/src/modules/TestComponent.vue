473a36f818296f6b7b4007f355e1db728dbc3a79
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-08-31 08:46:58 +0800

    SFC

---
+++ section06/test/src/modules/TestComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        テストです
+        {{ testData }}
+    </div>
+</template>
+<script>
+export default {
+    name: 'TestComponent',
+    data: function(){
+        return {
+            testData: 'テストdataです'
+        }
+    }
+}
+</script>
+<style>
+</style>

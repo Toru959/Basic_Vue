fcda9c7c550020976526955ee970c1f4cdac89e8
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-01 13:56:05 +0800

    Update .DS_Store and add vuerouter directory

---
+++ section07/vuerouter/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <button v-on:click="checkRouteInfo">ルート情報</button><br>
+    <button v-on:click="goToHome">Homeに戻る</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutTest',
+  methods: {
+    checkRouteInfo(){
+      console.log(this.$route)
+      console.log(this.$route.params.path)
+      console.log(this.$router)
+    },
+    goToHome(){
+      this.$router.push('/')
+    }
+  }
+
+}
+</script>

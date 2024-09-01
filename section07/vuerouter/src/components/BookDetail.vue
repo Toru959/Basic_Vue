b41908b8761564ad624c8ec52f727c323821efa7
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-02 04:59:54 +0800

    動的パラメータwatch

--- section07/vuerouter/src/components/BookDetail.vue
+++ section07/vuerouter/src/components/BookDetail.vue
@@ -3,6 +3,7 @@
        <p>本の詳細</p> 
        <p>タイトル: {{ title }}</p>
        <p>本の内容: {{ content }}</p>
+       {{ books[bookIndex].title  }}
     </div>
 </template>
 
@@ -13,8 +14,24 @@ export default {
         id: Number,
         title: String,
         content: String,
+    },
+    data: function(){ // リロードした時にBookDetailに情報がないとエラーになるので初期値として設定しておく
+        return {
+            bookIndex: this.$route.params.id -1,
+            books:[
+                {id:1, title:'タイトル１', content:'本の内容1'},
+                {id:2, title:'タイトル2', content:'本の内容2'},
+                {id:3, title:'タイトル3', content:'本の内容3'},
+            ]
+        }
+    },
+    created(){ // コンポーネントが読み込まれるタイミングで実行される
+        if(this.$route.params.id > this.books.length){
+            this.$router.push('/book') // 存在しないパラメータならbookに飛ばす
+        }
     }
 }
+    
 </script>
 
 <style>

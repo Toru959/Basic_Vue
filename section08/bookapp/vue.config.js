b3fd846f9eea5b63e7f2a554f1e6a38c317fb8e9
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-04 10:45:40 +0800

    bookapi

--- section08/bookapp/vue.config.js
+++ section08/bookapp/vue.config.js
@@ -2,5 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://www.googleapis.com',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
 })

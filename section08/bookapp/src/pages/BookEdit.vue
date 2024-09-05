22116e8030a32a8ea36e2820da1cfd80bed2627a
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-05 11:10:16 +0800

    bookEdit, Calendar

--- section08/bookapp/src/pages/BookEdit.vue
+++ section08/bookapp/src/pages/BookEdit.vue
@@ -1,6 +1,51 @@
 <template>
-    <div>編集
-        {{ books }}
+    <div>
+        <v-row>
+            <v-col cols="12">
+                <v-card class="mx-auto">
+                    <v-row>
+                        <v-col cols="4">
+                            <v-img v-bind:src="book.image"></v-img>
+                        </v-col>
+                    </v-row>
+                    <v-col cols="8">
+                        <v-card-title>
+                            タイトル:{{ book.title }}
+                        </v-card-title>
+                        読んだ日：<v-menu
+                        v-model="menu"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                    >
+                        <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                            v-model="date"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                        ></v-text-field>
+                        </template>
+                        <v-date-picker
+                        v-model="date"
+                        v-on:input="menu = false"
+                        locale="jp-ja"
+                        :day-format="date => new Date(date).getDate()"
+                        ></v-date-picker>
+                        </v-menu>
+                        感想：<v-textarea class="mx-2" v-model="book.memo">
+                            {{ book.memo }}
+                        </v-textarea>
+                        <v-card-actions>
+                            <v-btn color="secondary" to="/">一覧に戻る</v-btn>
+                            <v-btn color="info">保存する</v-btn>
+                        </v-card-actions>
+                    </v-col>
+                </v-card>
+            </v-col>
+        </v-row>
     </div>
 </template>
 
@@ -8,7 +53,21 @@
 export default {
     name: 'BookEdit',
     props:{
-        books:Array
+        books:Array,
+        date: new Date().toISOString().substr(0, 10),
+    },
+    data(){
+        return{
+            book: ''
+        }
+    },
+    beforeRouteEnter(to, from, next){
+        next( vm => {
+            vm.$nextTick(() => {
+                vm.book = vm.books[vm.$route.params.id]
+                // console.log(vm.book)
+            })
+        })
     }
 }
 </script>

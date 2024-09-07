075562ef97e2f67e402565633323f20b6aaf1e12
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-07 10:49:48 +0800

    state mutation payload

---
+++ section09/vuex/src/store/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: { // dataと同じ初期値
+    count: 0
+  },
+  getters: {
+  },
+  mutations: {
+    increment(state){
+      state.count++
+    },
+    addCount(state, payload){ // 第二引数はオブジェクト
+      state.count += payload.value
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})

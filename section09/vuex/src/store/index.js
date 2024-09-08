d9ba9a870a7374de5ac964723ae92b8976ff9164
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-08 10:05:03 +0800

    getters

--- section09/vuex/src/store/index.js
+++ section09/vuex/src/store/index.js
@@ -5,9 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: { // dataと同じ初期値
-    count: 0
+    count: 0,
+    users:[
+      {id:1, name:'大谷', isVisible: true },
+      {id:2, name:'ダルビッシュ', isVisible: false },
+      {id:3, name:'藤波', isVisible: true },
+    ]
   },
   getters: {
+    // visibleUsers(state){
+    //   return state.users.filter(user => user.isVisible)
+    // }
+    // アロー関数で書く場合、idを指定する場合の書き方
+    visibleUsers: state => state.users.filter(user => user.isVisible),
+    getUserById: state => id => {
+      return state.users.find(user => user.id === id)
+    }
   },
   mutations: {
     increment(state){

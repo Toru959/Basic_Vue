3a78f56599650b204957f4142462ad9811bb1cc9
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-09 09:48:14 +0800

    composition API

---
+++ section10/vue3/src/views/CompositionTest.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>コンポジションAPI</div>
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+    <p>{{ nameRef }}</p>
+    <div>{{ number }}</div>
+    <div>{{ sports }}</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+    
+    setup(){
+        // console.log('setup') // createdより早い
+        // console.log(this)
+
+        let name = '大谷'
+        const age = 30
+        const nameRef = ref('松阪') // refはsetupをリアクティブにする。returnで返すとtemplateで使用できる
+        console.log(nameRef)
+        console.log(nameRef.value)
+
+        return { // returnに書いた変数・関数をtemplate内で扱える
+            name, 
+            age,
+            nameRef
+        } 
+    },
+    data(){
+        return {
+            number: 1,
+            sports: '野球'
+        }
+    },
+    created(){
+        // console.log('created')
+        // console.log(this)
+    },
+    mounted(){
+        // console.log('mounted')
+    }
+}
+</script>
+
+<style>
+
+</style>

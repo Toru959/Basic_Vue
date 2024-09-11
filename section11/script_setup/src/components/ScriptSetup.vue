b7a0df913071fdeab2f3916978a9115e3fcf70ee
Toru959 <80747436+Toru959@users.noreply.github.com>
2024-09-11 09:53:59 +0800

    scritp setup

---
+++ section11/script_setup/src/components/ScriptSetup.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+    count.value++;
+}
+
+const decrement = () => {
+    count.value--;
+}
+</script>
+
+<template>
+    <h1>{{ count }}</h1>
+    <button v-on:click="increment">+</button>
+    <button v-on:click="decrement">-</button>
+
+</template>
+
+<style></style>
--- section11/script_setup/src/views/HomeView.vue
+++ section11/script_setup/src/views/HomeView.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ScriptSetup />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import ScriptSetup from '@/components/ScriptSetup'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    ScriptSetup
   }
 }
 </script>
